fc90aaab54be23b21a8ba6a99db40b88abad1328
0024hhh-oh <0024hhh@gmail.com>
2026-08-12 07:34:18 +0900

    Add subtle rain animation

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,26 @@
 import {Composition} from 'remotion';
 import {NightLightingLoop} from './NightLightingLoop';
+import {DayRainLoop} from './DayRainLoop';
 
 export const Root: React.FC = () => {
   return (
-    <Composition
-      id="NightLightingLoop"
-      component={NightLightingLoop}
-      durationInFrames={900}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Composition
+        id="NightLightingLoop"
+        component={NightLightingLoop}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="DayRainLoop"
+        component={DayRainLoop}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </>
   );
 };
